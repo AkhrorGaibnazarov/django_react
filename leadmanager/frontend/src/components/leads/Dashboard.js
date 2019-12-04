18bbf0acc1eb9e00b1a402a5df9b115e3f059dcb
Akhror <akhrorgaibnazarov@gmail.com>
2019-12-04 16:55:50 +0500

    Register, delete and others

--- leadmanager/frontend/src/components/leads/Dashboard.js
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -1,20 +1,14 @@
-import React, {
-    Fragment
-} from 'react';
-import Form from './Form';
-import Leads from './Leads';
+import React, { Fragment } from "react";
+import Form from "./Form";
+import Leads from "./Leads";
 
 export default function Dashboard() {
-    return ( <
-        div >
-        <
-        Fragment >
-        <
-        Form / >
-        <
-        Leads / >
-        <
-        /Fragment> <
-        /div>
-    )
+  return (
+    <div>
+      <Fragment>
+        <Form />
+        <Leads />
+      </Fragment>{" "}
+    </div>
+  );
 }
